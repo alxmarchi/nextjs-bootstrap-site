7d43005ad38e3690f4f6dd4b92e69fd8347f3429
alxmarchi <zyex@mail.ru>
2021-02-04 18:28:06 +0300

    fix and refactor

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,21 @@ import Layout from "../components/layout";
 export default function About() {
     return(
     <Layout>
-    <h1>О компании</h1>
+     <> 
+        <h1 className="m-5">Информация о компании</h1>
+    <CarouselBoxAbout></CarouselBoxAbout>
+    <div className="m-5">
+    <p>Sydler Remedies Pvt. Ltd. — Группа компаний Sydler — это ведущий производитель и экспортёр#nbsp; высококачественных и надежных лекарственных форм, фитопрепаратов, БАДов и пищевых добавок#nbsp;, а также нутрицевтиков, которые удовлетворяют взыскательные требования рынка по всему миру.</p>
+    <h3 className="m-4">Сертификаты</h3>
+    <Gallery/>
+    <h3 className="m-4">Контакты</h3>
+    <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+    
+    </div>
+    
+    
+
+</>
         </Layout>
     )
 }
